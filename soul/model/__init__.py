aafd0eec604ddbe3c9397133fe5f3d2e222675fd
amazingDD <1172392977@gmail.com>
2025-06-26 10:15:26 +0800

    add qkformer

--- soul/model/__init__.py
+++ soul/model/__init__.py
@@ -4,3 +4,4 @@ from .msresnet import MSResNet18, MSResNet34, MSResNet50
 from .spikformer import Spikformer256, Spikformer384, Spikformer512
 from .metaspikformer import MetaSpikformer256, MetaSpikformer384, MetaSpikformer512
 from .spikingresformer import SpikingResformer192, SpikingResformer384, SpikingResformer512, SpikingResformer256
+from .qkformer import QKFormer256, QKFormer384, QKFormer512
