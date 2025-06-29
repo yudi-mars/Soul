1395f2b98b5fedadf9a8d7545b6de6d39bdc2610
amazingDD <1172392977@gmail.com>
2025-06-29 17:00:42 +0800

    rename

--- soul/model/__init__.py
+++ soul/model/__init__.py
@@ -2,7 +2,7 @@ from .spikingvgg import SpikingVGG5, SpikingVGG9, SpikingVGG11, SpikingVGG13, Sp
 from .sewresnet import SEWResNet18, SEWResNet50, SEWResNet34
 from .msresnet import MSResNet18, MSResNet34, MSResNet50
 from .spikformer import Spikformer256, Spikformer384, Spikformer512
-from .metaspikformer import MetaSpikeformer256, MetaSpikeformer384, MetaSpikeformer512
+from .metaspikeformer import MetaSpikeformer256, MetaSpikeformer384, MetaSpikeformer512
 from .spikingresformer import SpikingResformer192, SpikingResformer384, SpikingResformer512, SpikingResformer256
 from .qkformer import QKFormer256, QKFormer384, QKFormer512
 
