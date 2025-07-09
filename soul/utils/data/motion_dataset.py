abb72f4a3bc98becc4aec6d2cd70381b0ab0d4f3
amazingDD <1172392977@gmail.com>
2025-07-09 22:28:08 +0800

    init abc class

--- soul/utils/data/motion_dataset.py
+++ soul/utils/data/motion_dataset.py
@@ -1,1 +1,16 @@
-# TODO
+class MotionData(object):
+    train_trsf = []
+    test_trsf = []
+    common_trsf = []
+
+    input_shape = None, None, None
+    num_classes = None
+
+    data_source = None
+
+    def __init__(self, data_dir, T):
+        self.data_dir = data_dir
+        self.T = T
+
+    def download_data(self):
+        raise NotImplementedError
