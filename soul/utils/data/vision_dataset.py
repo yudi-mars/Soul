6763b090e3af71d6ab805a4bc2f5c0510ace3c0e
amazingDD <1172392977@gmail.com>
2025-07-10 14:07:35 +0800

    add reference

--- soul/utils/data/vision_dataset.py
+++ soul/utils/data/vision_dataset.py
@@ -1,3 +1,19 @@
+'''
+Filename: vision_dataset.py
+Author: Di Yu <yudi2023@zju.edu.cn>
+Date Created: 2025-07-10
+Description:
+    Load data from motion sensor with some data augmentation operations
+
+References:
+    - Wei Fang et al., "SpikingJelly: An open-source machine learning infrastructure platform for spike-based intelligence", 2023.
+    https://github.com/fangwei123456/spikingjelly
+    - Da-Wei Zhou et al., "Class-Incremental Learning: A Survey", 2024.
+    https://github.com/LAMDA-CL/PyCIL
+    - Li, Yuhang et al., "Neuromorphic Data Augmentation for Training Spiking Neural Networks", 2022.
+    https://github.com/Intelligent-Computing-Lab-Panda/NDA_SNN
+    
+'''
 import os
 import time
 import math
