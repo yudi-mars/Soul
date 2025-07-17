e047647cf37ea82220a41d2c3ad106325860ec7b
amazingDD <1172392977@gmail.com>
2025-07-17 17:32:23 +0800

    init dana for har

---
+++ soul/model/motion/dana.py
@@ -0,0 +1,11 @@
+"""
+Filename: dana.py
+Author: Di Yu <yudi2023@zju.edu.cn>
+Date Created: 2025-07-17
+Description:
+    implementation for spike-wise DANA for HAR.
+
+References:
+    - Mohammad M. et al., "DANA: Dimension-Adaptive Neural Architecture for Multivariate Sensor Data", UbiComp'2021.
+      https://github.com/mmalekzadeh/dana
+"""
