7f3ed6f56cc1ceca05138b0cc5be2e6b98f18ea0
amazingDD <1172392977@gmail.com>
2025-11-19 12:46:48 +0800

    only preserve dropout+noise currently

--- soul/utils/noise/__init__.py
+++ soul/utils/noise/__init__.py
@@ -7,21 +7,17 @@ noising_map = {
     'wireless': {
         'gaussian': wn.add_gaussian_noise,
         'dropout': wn.add_dropouts_noise,
-        'impulse': wn.add_impulse_noise,
     },
     'acoustic': {
         'gaussian': an.add_gaussian_noise,
         'dropout': an.add_dropouts_noise,
-        'impulse': an.add_impulse_noise,
     },
     'motion': {
-        'dropout': mn.add_dropouts_noise,
         'gaussian': mn.add_gaussian_noise,
-        'impulse': mn.add_impulse_noised,
+        'dropout': mn.add_dropouts_noise,
     },
     'vision': {
         'gaussian': vn.add_gaussian_noise,
-        'impluse': vn.add_impulse_noise,
         'dropout': vn.add_dropouts_noise,
     }
 }
