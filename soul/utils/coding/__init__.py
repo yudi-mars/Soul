6d65b28db3ad31e4122c7085fe1898e928b01eef
zws <2985693012@qq.com>
2025-12-07 03:18:17 +0000

    updata conding init

--- soul/utils/coding/__init__.py
+++ soul/utils/coding/__init__.py
@@ -7,14 +7,13 @@ from .phase import encode as phase_encode
 from .poisson import encode as poisson_encode
 from .population import encode as population_encode
 from .rank_order import encode as rank_order_encode
-from .temporal import encode as temporal_encode
 from .rate import encode as rate_encode
 from .sdr import encode as sdr_encode
 from .tcr_mw import encode as tcr_mwc_encode
 from .tcr_sf import encode as tcr_sf_decode
 from .tcr_tbr import encode as tcr_tbr_encode
-from .temporal import encode as temporal_encode
-
+from .ttfs import encode as ttfs_encode
+from .tsc import encode as tsc_encode
 
 coding_map = {
     'binary_parallel': binary_par_encode,
@@ -26,10 +25,15 @@ coding_map = {
     'poisson': poisson_encode,
     'population': population_encode,
     'rank_order': rank_order_encode,
-    'temporal': temporal_encode,
+    'ttfs': ttfs_encode,
     'rate': rate_encode,
     'sdr': sdr_encode,
     'tcr_mw': tcr_mwc_encode,
     'tcr_sf': tcr_sf_decode,
     'tcr_tbr': tcr_tbr_encode,
+    'tsc': tsc_encode,
 }
+'''
+binary_parallel、binary_sequential、bsa、burst、phase、rank_order、ttfs、sdr、tcr_mw、tcr_sf、tcr_tbr、tsc
+direct、poisson、population、rate、
+'''
